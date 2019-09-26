1803d006e5e49837ad24e76790bc88565451197e
Tim Morse <sasessiontc@gmail.com>
2019-09-26 10:55:28 -0400

    Initial commit

--- Tests/PactSafeTests/PactSafeTests.swift
+++ Tests/PactSafeTests/PactSafeTests.swift
@@ -6,7 +6,7 @@ final class PactSafeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PactSafe().text, "Hello, World!")
+//        XCTAssertEqual(PactSafe().text, "Hello, World!")
     }
 
     static var allTests = [
